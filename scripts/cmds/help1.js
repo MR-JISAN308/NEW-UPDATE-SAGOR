12b21148397992821bde47d966a239721ad988bf
MR-JISAN308 <mrjisan54443@gmail.com>
2024-11-21 18:38:59 +0600

    Update help1.js

--- scripts/cmds/help1.js
+++ scripts/cmds/help1.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | 🇸 🇦 🇬 🇴 🇷  V2 ]";
+const doNotDelete = "[ 💚 | 🇯 🇮 🇸 🇦 🇳   V2 ]";
 
 module.exports = {
 	config: {
@@ -34,7 +34,7 @@ module.exports = {
 			const categories = {};
 			let msg = "";
 
-			msg += `╔══════════════╗\n     🇸 🇦 🇬 🇴 🇷  CMD💐\n╚══════════════╝`;
+			msg += `╔══════════════╗\n     🇯 🇮 🇸 🇦 🇳   CMD💐\n╚══════════════╝`;
 
 			for (const [name, value] of commands) {
 					if (value.config.role > 1 && role < value.config.role) continue;
@@ -61,7 +61,7 @@ module.exports = {
 			const totalCommands = commands.size;
 			msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
 			msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`;
-			msg += `🇸 🇦 🇬 🇴 🇷  🇦 🇮  🇨 🇭 🇦 🇹 🇧 🇴 🇹 
+			msg += `߷ 𝑱𝑰𝑺𝑨𝑵 𝑯𝑬𝑳𝑷 𝑳𝑰𝑺𝑻 ߷ 
 		 💐`;
 
 
