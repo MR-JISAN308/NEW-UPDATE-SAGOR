9c44749bc18db749146e06f0d56b1dbef8392aef
MR-JISAN308 <mrjisan54443@gmail.com>
2024-11-22 15:39:14 +0600

    Update help1.js

--- scripts/cmds/help1.js
+++ scripts/cmds/help1.js
@@ -50,7 +50,7 @@ module.exports = {
 
 							const names = categories[category].commands.sort();
 							for (let i = 0; i < names.length; i += 1) {
-									const cmds = names.slice(i, i + 1).map(item => `│🎀${item}💙`);
+									const cmds = names.slice(i, i + 1).map(item => `│🩷${item}🪽`);
 									msg += `\n${cmds.join(" ".repeat(Math.max(0, 5 - cmds.join("").length)))}`;
 							}
 
